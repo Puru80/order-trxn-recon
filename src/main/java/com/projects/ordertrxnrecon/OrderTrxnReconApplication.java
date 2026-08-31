e67ad9047e8086869d537ed174431471f2d2d721
Puru80 <puru.agar99@gmail.com>
2026-08-31 16:46:17 +0530

    Initial Commit

---
+++ src/main/java/com/projects/ordertrxnrecon/OrderTrxnReconApplication.java
@@ -0,0 +1,13 @@
+package com.projects.ordertrxnrecon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderTrxnReconApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderTrxnReconApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/ordertrxnrecon/OrderTrxnReconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.ordertrxnrecon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderTrxnReconApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
